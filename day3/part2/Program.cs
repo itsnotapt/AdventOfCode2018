c941c39441c2b78833bd1b5074dcef366e848efa
itsnotapt <3096198+itsnotapt@users.noreply.github.com>
2018-12-03 23:00:48 +0000

    Create Program.cs

---
+++ day3/part2/Program.cs
@@ -0,0 +1,94 @@
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace day3part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fabric f = new Fabric();
+            foreach (string line in File.ReadLines(@"input.txt"))
+            {
+                f.addLine(line);
+            }
+            foreach (string line in File.ReadLines(@"input.txt"))
+            {
+                int id = f.checkClaim(line);
+                if (id >= 0)
+                {
+                    Console.WriteLine("Result: {0}", id);
+                }
+            }
+            
+        }
+    }
+
+    class Fabric
+    {
+        private int[,] area = new int[1000, 1000];
+        private Regex rx = new Regex(@"^#(\d{1,4}) @ (\d{1,3}),(\d{1,3}): (\d{1,2})x(\d{1,2})", RegexOptions.Compiled);
+
+        public void addLine(string line)
+        {
+            Match match = rx.Match(line);
+            int id = Int32.Parse(match.Groups[1].Value);
+            int leftEdge = Int32.Parse(match.Groups[2].Value);
+            int topEdge = Int32.Parse(match.Groups[3].Value);
+            int width = Int32.Parse(match.Groups[4].Value);
+            int height = Int32.Parse(match.Groups[5].Value);
+
+            for (int x = leftEdge; x < (leftEdge + width); x++)
+            {
+                for (int y = topEdge; y < (topEdge + height); y++)
+                {
+                    area[x, y]++;
+                }
+            }
+        }
+
+        public int checkClaim(string line)
+        {
+            Match match = rx.Match(line);
+            int id = Int32.Parse(match.Groups[1].Value);
+            int leftEdge = Int32.Parse(match.Groups[2].Value);
+            int topEdge = Int32.Parse(match.Groups[3].Value);
+            int width = Int32.Parse(match.Groups[4].Value);
+            int height = Int32.Parse(match.Groups[5].Value);
+
+            bool noOverlap = true;
+            for (int x = leftEdge; x < (leftEdge + width); x++)
+            {
+                for (int y = topEdge; y < (topEdge + height); y++)
+                {
+                    if (area[x,y] != 1)
+                    {
+                        noOverlap = false;
+                    }
+                }
+            }
+            if (noOverlap)
+            {
+                return id;
+            }
+            return -1;
+        }
+
+        public int countSquares()
+        {
+            int count = 0;
+            for (int x = 0; x < area.GetLength(0); x++)
+            {
+                for (int y = 0; y < area.GetLength(1); y++)
+                {
+                    if (area[x, y] >= 2)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
